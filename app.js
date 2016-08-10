80ca9baa6638e9c9a37e61beb5bfb6b9cd9abbea
Zichao Yan <yanzichao.cn@163.com>
2016-08-10 18:27:30 +0800

    fixes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const http         = require('http'),
       MongoClient  = require('mongodb').MongoClient;
 
 fs.open("log.txt","a",0x0644, function(err, fd){
-  fs.write(fd, `application ${process.pid} initiated \r\n`,0,'utf8',function(e){
+  fs.write(fd, "application ${process.pid} initiated \r\n",0,'utf8',function(e){
     if(e) throw e;
   });
 
@@ -50,35 +50,18 @@ fs.open("log.txt","a",0x0644, function(err, fd){
 //console.log("env.NODE_IP "   + env.NODE_IP);
 
   server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-    fs.write(fd, `server on application ${process.pid} started, running at ip ${env.NODE_IP} port ${env.NODE_PORT}\r\n`,
+    fs.write(fd, "server on application ${process.pid} started, running at ip ${env.NODE_IP} port ${env.NODE_PORT}\r\n",
         0,'utf8',function(e){
       if(e) throw e;
     });
   });
 
 
-  var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
-  var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
-
-  console.log("process.env.OPENSHIFT_NODEJS_IP : " + env.OPENSHIFT_NODEJS_IP);
-  console.log("process.env.OPENSHIFT_NODEJS_PORT : "+ env.OPENSHIFT_NODEJS_PORT);
+  //var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
+  //var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 
 // default to a 'localhost' configuration:
-  var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
-// if OPENSHIFT env variables are present, use the available connection info:
-  if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    console.log("process.env.OPENSHIFT_MONGODB_DB_USERNAME : " + env.OPENSHIFT_MONGODB_DB_USERNAME);
-    console.log("process.env.OPENSHIFT_MONGODB_DB_PASSWORD : " + env.OPENSHIFT_MONGODB_DB_PASSWORD);
-    console.log("process.env.OPENSHIFT_MONGODB_DB_HOST : "     + env.OPENSHIFT_MONGODB_DB_HOST);
-    console.log("process.env.OPENSHIFT_MONGODB_DB_PORT : "     + env.OPENSHIFT_MONGODB_DB_PORT);
-    console.log("process.env.OPENSHIFT_APP_NAME : "            + env.OPENSHIFT_APP_NAME);
-
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-  }
+  var connection_string = '127.0.0.1:27017/coordinates';
 
   var i= 0 ,j = 0;
 // the client db connection scope is wrapped in a callback:
