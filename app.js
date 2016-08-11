ab339a0480f1dc70b3f04b114eef59c2a4526367
Zichao Yan <yanzichao.cn@163.com>
2016-08-11 13:22:52 +0800

    fixes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ fs.open("log.txt","a",0x0644, function(err, fd){
 
   app.listen(port,ip, function (err) {
     if (err)  console.log(err);
-    else console.log("listenting at: %j",server.address());
+    else console.log("listenting at: %j",app.address());
 
     fs.write(fd, `server on application ${process.pid} started, running at ip ${ip} port ${port}\r\n`,
         0,'utf8',function(e){
