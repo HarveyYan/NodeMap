c35ea655b8ed6f7f32babdfe5f58de8944066c7b
Zichao Yan <yanzichao.cn@163.com>
2016-09-24 00:08:02 +0800

    second phase

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ fs.open(__dirname+"/log.txt","a",0x0644, function(err, fd){
       if (new_entries.length!=0) {
         //全部的json重新生成，在此可以进行一些优化
         var opts = {stdio: 'inherit'} ;
-        var javac = spawn('javac', ['-cp', '/usr/local/nodejsapp/app/Java_modules/lib/\*', '/usr/local/nodejsapp/app/Java_modules/src/\*.java'], opts);
+        var javac = spawn('javac', ['-cp', '/usr/local/nodejsapp/app/Java_modules/lib/\*', '/usr/local/nodejsapp/app/Java_modules/src/Change.java','/usr/local/nodejsapp/app/Java_modules/src/BaiduApi.java','/usr/local/nodejsapp/app/Java_modules/src/StuService.java'], opts);
 
         javac.on('close', function (code) {
           if (code === 0) {
