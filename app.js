9b0f5edf5420d5e8d6be417fd0779451b38fcd7a
Zichao Yan <yanzichao.cn@163.com>
2016-09-24 00:11:22 +0800

    second phase

--- app.js
+++ app.js
@@ -55,15 +55,15 @@ fs.open(__dirname+"/log.txt","a",0x0644, function(err, fd){
         var opts = {stdio: 'inherit'} ;
         var javac = spawn('javac', ['-cp', '/usr/local/nodejsapp/app/Java_modules/lib/\*', '/usr/local/nodejsapp/app/Java_modules/src/Change.java','/usr/local/nodejsapp/app/Java_modules/src/BaiduApi.java','/usr/local/nodejsapp/app/Java_modules/src/StuService.java'], opts);
 
-        javac.on('close', function (code) {
-          if (code === 0) {
-            var javaa  = spawn('java', [' -cp "Java_modules:Java_modules/lib/\* src.Change"'], opts);
-          }
+        javac.on('close', function () {
+          var javaa  = spawn('java', ['-cp','Java_modules:Java_modules/lib/\*','src.Change'], opts);
+            javaa.on('close',function(){
+              send_html(date);
+            });
         });
         //execSync('javac -cp Java_modules/lib/\* Java_modules/src/*.java',{stdio:[0,1,2]});
         //execSync('java -cp Java_modules:Java_modules/lib/\* src.Change',{stdio:[0,1,2]});
       }
-      send_html(date);
   }
 
   function send_html(date){
