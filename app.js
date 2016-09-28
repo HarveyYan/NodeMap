7127c0c8c21fc307b3098b21769b540455393021
Zichao Yan <yanzichao.cn@163.com>
2016-09-28 19:15:30 +0800

    fixes

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ fs.open(__dirname+"/log.txt","a",0x0644, function(err, fd){
         }
       }
       fs.write(fd, new_entries);
-      send_html(date,res);
       if (new_entries.length!=0) {
         var opts = {stdio: 'inherit'} ;
         var javac = spawn('javac', ['-cp', '/usr/local/nodejsapp/app/Java_modules/lib/\*', '/usr/local/nodejsapp/app/Java_modules/src/Change.java','/usr/local/nodejsapp/app/Java_modules/src/BaiduApi.java','/usr/local/nodejsapp/app/Java_modules/src/StuService.java'], opts);
@@ -68,6 +67,8 @@ fs.open(__dirname+"/log.txt","a",0x0644, function(err, fd){
         });
         //execSync('javac -cp Java_modules/lib/\* Java_modules/src/*.java',{stdio:[0,1,2]});
         //execSync('java -cp Java_modules:Java_modules/lib/\* src.Change',{stdio:[0,1,2]});
+      }else{
+        send_html(date,res);
       }
 
   }
